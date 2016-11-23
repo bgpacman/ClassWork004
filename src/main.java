e3e6d2d4585969dd266087978f9a801983600ef6
Lubcho Milushev <lubcho.milushev@gmail.com>
2016-11-23 13:08:54 +0200

    ClassWork004

---
+++ src/main.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		/*
+		 * Íŕďčřĺňĺ ďđîăđŕěŕ, ęî˙ňî âçĺěŕ äŕäĺíŕ öčôđŕ (0-9) č ˙ čçďčńâŕ ń äóěŕ
+		 * (íŕ ŕíăë. ĺçčę)
+		 */
+//		System.out.println("Input a digit from 0 to 9: ");
+//		String str1 = in.nextLine();
+//		switch (str1) {
+//		case "0":
+//			System.out.println("Zero");
+//			break;
+//		case "1":
+//			System.out.println("One");
+//			break;
+//		case "2":
+//			System.out.println("Two");
+//			break;
+//		case "3":
+//			System.out.println("Three");
+//			break;
+//		case "4":
+//			System.out.println("Four");
+//			break;
+//		case "5":
+//			System.out.println("Five");
+//			break;
+//		case "6":
+//			System.out.println("Six");
+//			break;
+//		case "7":
+//			System.out.println("Seven");
+//			break;
+//		case "8":
+//			System.out.println("Eight");
+//			break;
+//		case "9":
+//			System.out.println("Nine");
+//			break;
+//		default:
+//			System.out.println("Invalid input");
+//			break;
+//		}
+		
+		/*
+		 * Íŕďčřĺňĺ ďđîăđŕěŕ, ęî˙ňî čç÷čńë˙âŕ áîíóń ňî÷ęč íŕ áŕçŕ đĺçóëňŕň îň
+		 * čăđŕ. Ďîňđĺáčňĺë˙ň ňđ˙áâŕ äŕ âúâĺäĺ ńâîčňĺ ňî÷ęč â číňĺđâŕëa[1;9].
+		 * Ŕęî ňî÷ęčňĺ ńŕ â číňĺđâŕëŕ [1;3], óěíîćĺňĺ ăč ďî 5. Ŕęî ňî÷ęčňĺ ńŕ â
+		 * číňĺđâŕëŕ [4;6], óěíîćĺňĺ ăč ďî 10. Ŕęî ňî÷ęčňĺ ńŕ â číňĺđâŕëŕ [7;9],
+		 * óěíîćĺňĺ ăč ďî 50. Đĺçóëňŕňúň čçâĺäĺňĺ íŕ ĺęđŕíŕ
+		 */		
+		// System.out.println("Enter points in the interval of [0-9]");
+		// int examScore = in.nextInt();
+		// switch (examScore) {
+		// case 1:
+		// case 2:
+		// case 3:
+		// examScore = examScore * 5;
+		// break;
+		// case 4:
+		// case 5:
+		// case 6:
+		// examScore = examScore * 10;
+		// break;
+		// case 7:
+		// case 8:
+		// case 9:
+		// examScore = examScore * 50;
+		// break;
+		// default:
+		// System.out.println("Invalid input.");
+		// break;
+		// }
+		// System.out.println("Your score is: " + examScore);
+		
+		/*
+		 * Ęëŕńč÷ĺńęîňî ňĺńňĺ ęŕđňč ńĺ ńúńňîč îň 13 đŕçëč÷íč âčäŕ ęŕđňč
+		 * ńúîňâĺňíî 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K and A. Íŕďđŕâĺňĺ
+		 * ďđîăđŕěŕ, ęî˙ňî ďđčĺěŕ îň ęîíçîëŕňŕ ĺäčí ńčěâîë č ďđčíňčđŕ
+		 * "Âŕëčäíŕ ęŕđňŕ", ŕęî ńčěâîëúň ďđĺäńňŕâë˙âŕ âŕëčäíŕ ęŕđňŕ, č ńúîňâĺňíî
+		 * ďđčíňčđŕ "Íĺâŕëčäíŕ ęŕđňŕ", ŕęî ńčěâîëúň ĺ íŕ íĺâŕëčäíŕ ęŕđňŕ.
+		 */
+		// System.out.println("Choose card: ");
+		// char card = in.next().charAt(0);
+		// if (card == '2' || card == '3' || card == '4' || card == '5' || card
+		// == '6'
+		// || card == '7' || card == '8' || card == '9' || card == 'T'
+		// || card == 'J' || card == 'Q' || card == 'K' || card == 'A'){
+		// System.out.println("Valid card.");
+		// } else {
+		// System.out.println("Invalid card.");
+		// }
+				
+	}
+
+}
